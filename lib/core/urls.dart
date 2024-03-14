4313f4601b163e2b56411396421314fc1baa0d1a
adikagit <adikagit00@gmail.com>
2024-03-14 14:09:47 +0600

    ppre

--- lib/core/urls.dart
+++ lib/core/urls.dart
@@ -1,6 +1,8 @@
 class DocFF {
-  static const String slkdnvsdvsdv = '';
-  static const String pP = 'https://pub.dev/';
-  static const String tUse = 'https://pub.dev/';
-  static const String s = 'https://pub.dev/';
+  static const String slkdnvsdvsdv = 'placement_117';
+  static const String pP =
+      'https://docs.google.com/document/d/1TXSBy0-N86KBumUtnPTgRyvto2o0jBcnJak8jx3EF4U/edit?usp=sharing';
+  static const String tUse =
+      'https://docs.google.com/document/d/1oSrQPqNav3nwEFgb1IEyGkmht09KTcljtYcWoARXyjQ/edit?usp=sharing';
+  static const String s = 'https://forms.gle/KBSc9LrAL4r2d5Kq7';
 }
